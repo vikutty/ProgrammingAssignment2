1ca543477c6f1423be9a005f12e37c1eb5dc745c
Vinodh Sudhakaran <vikutty@gmail.com>
2019-08-12 22:24:06 +0800

    First draft of the program
    - Assumption: matrix input is of natural inverse in nature
    - makeCacheMatrix is forced to output the inverse value for cacheSolve to use
    - If cacheSolve does not receive inverse input from makeCacheMatrix, it generates its own inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Program for
 
-## Write a short comment describing this function
+## makeCacheMatrix function records and caches the inverse of the matrix provided
+## Assumption is the matrix provided is inverse compatible
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function() {
+    m <<- solve(x)
+  }
+  getinv <<- function() m
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  setinv()
+  # getinv()
 }
 
+## cacheSolve function tries to capture the inverse of matrix from makeCacheMatrix
+## If value not available then the function generates the inverse based on input variable
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, m, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  i <<- getinv()
+  }
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  else {
+    message("getting the inverse of the matrix")
+    data <- x$get()
+    i <- solve(data)
+    return(i)
+  }
 }
